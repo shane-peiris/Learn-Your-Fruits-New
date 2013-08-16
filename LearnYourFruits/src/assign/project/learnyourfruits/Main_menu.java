fdd4d7c3b55d19f592944d829bb0a19d9da1be87
Shane Peiris <mail.me.shane@gmail.com>
2013-08-16 17:15:53 +0530

    Main Menu changes and addition done..

--- LearnYourFruits/src/assign/project/learnyourfruits/Main_menu.java
+++ LearnYourFruits/src/assign/project/learnyourfruits/Main_menu.java
@@ -16,8 +16,40 @@ public class Main_menu extends Activity {
 		
 		TextView tv1 = (TextView) findViewById(R.id.txt_select_title);
 		
+		TextView[] tv=new TextView[21];
+		
+		tv[0] = (TextView) findViewById(R.id.txt_f_name1);
+		tv[1] = (TextView) findViewById(R.id.txt_f_name2);
+		tv[2] = (TextView) findViewById(R.id.txt_f_name3);
+		tv[3] = (TextView) findViewById(R.id.txt_f_name4);
+		tv[4] = (TextView) findViewById(R.id.txt_f_name5);
+		tv[5] = (TextView) findViewById(R.id.txt_f_name6);
+		tv[6] = (TextView) findViewById(R.id.txt_f_name7);
+		tv[7] = (TextView) findViewById(R.id.txt_f_name8);
+		tv[8] = (TextView) findViewById(R.id.txt_f_name9);
+		tv[9] = (TextView) findViewById(R.id.txt_f_name10);
+		tv[10] = (TextView) findViewById(R.id.txt_f_name11);
+		tv[11] = (TextView) findViewById(R.id.txt_f_name12);
+		tv[12] = (TextView) findViewById(R.id.txt_f_name13);
+		tv[13] = (TextView) findViewById(R.id.txt_f_name14);
+		tv[14] = (TextView) findViewById(R.id.txt_f_name15);
+		tv[15] = (TextView) findViewById(R.id.txt_f_name16);
+		tv[16] = (TextView) findViewById(R.id.txt_f_name17);
+		tv[17] = (TextView) findViewById(R.id.txt_f_name18);
+		tv[18] = (TextView) findViewById(R.id.txt_f_name19);
+		tv[19] = (TextView) findViewById(R.id.txt_f_name20);
+		tv[20] = (TextView) findViewById(R.id.txt_f_name21);
+		
+		
+		
 		Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/comic.ttf");		
+	
 		tv1.setTypeface(tf);
+		
+		for (int i=0; i<21;i++)
+		{
+			tv[i].setTypeface(tf);			
+		}
 	}
 
 	@Override
